45fe3822cd9e62b712baf0f04c5583b0afd933b5
Luddoz <ludovic.chombeau@gmail.com>
2019-03-20 10:44:39 +0100

    Created the 2 files

---
+++ main-M.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 10:35:33 2019
+
+@author: lxxdoo
+"""
+
+
+class blob:
+    def __init__(self, name, size, color):
+        self.name = name
+        self.size = size
+        self.color = color
+    
